cb1fada60d419a32cd6d9b562b92bfb40cdfc8cf
Malisa Lo <malisalo1703@gmail.com>
2025-05-05 21:48:21 -1000

    template

---
+++ README.md
@@ -0,0 +1,10 @@
+# DeniedSoft
+Website card for a start-up project.
+
+# View
+Appearance of the page.
+
+![alt text](https://raw.githubusercontent.com/DeniedAccessLife/DeniedSoft/master/view.png)
+
+# License
+This project is licensed under the GNU License - see the [LICENSE](LICENSE) file for details.
---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () =>
+    {
+      const accordionItems = document.getElementsByClassName('accordion__item');
+    
+      for (let i = 0; i < accordionItems.length; i++)
+      {
+        accordionItems[i].addEventListener('click', function()
+        {
+          const accordionContent = this.querySelector('.accordion__content');
+    
+          if (this.classList.contains('active'))
+          {
+            this.classList.remove('active');
+            accordionContent.style.maxHeight = null;
+          }
+          else
+          {
+            this.classList.add('active');
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+          }
+        });
+      }
+    });
